bf3ff9cb883717344ebb8b5236278a35f868aff4
Wilfried van Asten <wilfriedvanasten@gmail.com>
2016-03-04 23:59:08 +0100

    Add most, if not all, Haste.DOM functions
    Also corrected some naming and assure all
    exported symbols are inlinable. All symbols
    can also be safely exported with the new
    name mangling

--- src/Haste/Dome.hs
+++ src/Haste/Dome.hs
@@ -1,20 +1,4 @@
-module Haste.Dome (
-  eBuild,
-  eRunIn,
-  eApply,
-  eAppendChild,
-  eAppendChildM,
-  eAppendBuild,
-  eInsertChildBefore,
-  eWithQS,
-  eWithQS_,
-  (=:),
-  attr,
-  eSet,
-  newTextElem,
-  newElem,
-  eClearChildren
-) where
+module Haste.Dome where
 
 import Haste.DOM
 import Control.Monad.Reader
@@ -45,6 +29,7 @@ This version of build does not return the environment,
 since it would be returned to the source of the element
 which means it is already there
 -}
+{-# INLINABLE eRunIn #-}
 eRunIn :: (MonadIO m, IsElem e) => e -> Dome m a -> m ()
 eRunIn e t = runReaderT (void t) (elemOf e)
 
@@ -69,6 +54,7 @@ eAppendChild e = eApply (`Haste.DOM.appendChild` elemOf e)
 eAppendChildM :: (MonadIO m, IsElem e) => m e -> Dome m ()
 eAppendChildM e = lift e >>= eAppendChild
 
+{-# INLINABLE eAppendBuild #-}
 eAppendBuild :: MonadIO m => String -> Dome m a -> Dome m ()
 eAppendBuild s = eAppendChildM . eBuild s
 
@@ -77,30 +63,85 @@ Insert before takes a function that gets a child element from the
 environment and than inserts the given element before that element.
 -}
 {-# INLINABLE eInsertChildBefore #-}
-eInsertChildBefore :: (MonadIO m, IsElem before, IsElem child) => (Elem -> m before) -> child -> Dome m ()
-eInsertChildBefore f e = do
-  b <- eApply f
-  eApply (\p -> insertChildBefore p b e)
+eInsertChildBefore :: (MonadIO m, IsElem before, IsElem child) => before -> child -> Dome m ()
+eInsertChildBefore b e = eApply (\p -> insertChildBefore p b e)
 
 {-
 withQS executes the given action on the elements returned when the
 given query selector is run within the environment element. The
 results are gathered and returned as a list. See also withQS_
 -}
-{-# INLINABLE eWithQS #-}
-eWithQS :: MonadIO m => QuerySelector -> (Elem -> m a) -> Dome m [a]
-eWithQS qs f = do
-  es <- eApply (`elemsByQS` qs)
-  lift $ mapM f es
+{-# INLINABLE eMapQS #-}
+eMapQS :: MonadIO m => QuerySelector -> (Elem -> m a) -> Dome m [a]
+eMapQS qs f = eApply (\p -> mapQS p qs f)
 
-{-# INLINABLE eWithQS_ #-}
-eWithQS_ :: MonadIO m => QuerySelector -> (Elem -> m a) ->  Dome m ()
-eWithQS_ qs f = do
-  es <- eApply (`elemsByQS` qs)
-  lift $ mapM_ f es
+{-# INLINABLE eMapQS_ #-}
+eMapQS_ :: MonadIO m => QuerySelector -> (Elem -> m a) ->  Dome m ()
+eMapQS_ qs f = eApply (\p -> mapQS_ p qs f)
 
+eWithElemsQS :: MonadIO m => QuerySelector -> ([Elem] -> m a) -> Dome m a
+eWithElemsQS qs f = eApply (\p -> withElemsQS p qs f)
+
+{-# INLINABLE eSet #-}
 eSet :: MonadIO m => [Attribute] -> Dome m ()
 eSet as = eApply (`Haste.DOM.set` as)
 
+{-# INLINABLE eClearChildren #-}
 eClearChildren :: MonadIO m => Dome m ()
 eClearChildren = eApply clearChildren
+
+{-# INLINABLE eClick #-}
+eClick :: MonadIO m => Dome m ()
+eClick = eApply click
+
+{-# INLINABLE eFocus #-}
+eFocus :: MonadIO m => Dome m ()
+eFocus = eApply focus
+
+{-# INLINABLE eBlur #-}
+eBlur :: MonadIO m => Dome m ()
+eBlur = eApply blur
+
+{-# INLINABLE eGetFirstChild #-}
+eGetFirstChild :: MonadIO m => Dome m (Maybe Elem)
+eGetFirstChild = eApply getFirstChild
+
+{-# INLINABLE eGetLastChild #-}
+eGetLastChild :: MonadIO m => Dome m (Maybe Elem)
+eGetLastChild = eApply getLastChild
+
+{-# INLINABLE eGetChildren #-}
+eGetChildren :: MonadIO m => Dome m [Elem]
+eGetChildren = eApply getChildren
+
+{-# INLINABLE eSetChildren #-}
+eSetChildren :: MonadIO m => [Elem] -> Dome m ()
+eSetChildren es = eApply (`setChildren` es)
+
+{-# INLINABLE eDeleteChild #-}
+eDeleteChild :: MonadIO m => Elem -> Dome m ()
+eDeleteChild e = eApply (`deleteChild` e)
+
+{-# INLINABLE eElemsByQS #-}
+eElemsByQS :: MonadIO m => QuerySelector -> Dome m [Elem]
+eElemsByQS qs = eApply (`elemsByQS` qs)
+
+{-# INLINABLE eGetStyle #-}
+eGetStyle :: MonadIO m => PropID -> Dome m String
+eGetStyle propID = eApply (`getStyle` propID)
+
+{-# INLINABLE eSetStyle #-}
+eSetStyle :: MonadIO m => PropID -> String -> Dome m ()
+eSetStyle propID v = eApply (\p -> setStyle p propID v)
+
+{-# INLINABLE eSetClass #-}
+eSetClass :: MonadIO m => String -> Bool -> Dome m ()
+eSetClass n v = eApply (\p -> setClass p n v)
+
+{-# INLINABLE eToggleClass #-}
+eToggleClass :: MonadIO m => String -> Dome m ()
+eToggleClass n = eApply (`toggleClass` n)
+
+{-# INLINABLE eHasClass #-}
+eHasClass :: MonadIO m => String -> Dome m Bool
+eHasClass n = eApply (`hasClass` n)
